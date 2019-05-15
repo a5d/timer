ea19f190f2b5e695f28469b5796c7422af6791e5
evgeniy <5108591@mail.ru>
2019-05-15 23:23:58 +0300

    app interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './app.css'
 
 const App = () => {
-  return <div>123</div>
+  return <div className="app">
+    <div className="timer">00:00</div>
+    <div className="button"><button>Старт</button></div>
+    <div className="input"><input placeholder="Enter name" type="text"/></div>
+    <div className="table">
+      <table>
+        <tr>
+          <td>Task name</td>
+          <td>Start</td>
+          <td>Duration</td>
+          <td>End</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 }
 
 export default App
