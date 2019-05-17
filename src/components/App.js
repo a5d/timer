58d9e805c156e74629d62ab896fa80cbf835bf26
User <test@test.com>
2019-05-17 10:43:38 +0300

    fix time

--- src/components/App.js
+++ src/components/App.js
@@ -144,7 +144,7 @@ class App extends Component {
       <h1>Task #{taskId}</h1>
       <p>Task name: </p>
       <p>Start: {moment(task.timeStart).format('HH:mm:ss')}</p>
-      <p>Duration: {moment(task.timeEnd - task.timeStart).format('mm:ss')}</p>
+      <p>Duration: {moment(moment(task.timeEnd) - moment(task.timeStart)).format('mm:ss')}</p>
       <p>End: {moment(task.timeEnd).format('HH:mm:ss')}</p>
     </div>
   }
